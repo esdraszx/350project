58fe32ed6976322a64059a1b2b64cacb48e23539
esdraszx <43818958+esdraszx@users.noreply.github.com>
2021-03-14 23:27:48 -0600

    Update login.sql

--- login.sql
+++ login.sql
@@ -27,20 +27,42 @@ $$
 
 -- ADMIN LOGIN
 
-CREATE OR REPLACE FUNCTION
-login_admin(username TEXT, upassword TEXT) RETURNS jwt_token
-  LANGUAGE plpgsql SECURITY DEFINER
-  AS $$
+CREATE OR REPLACE FUNCTION public.login_admin(IN username text,IN upassword text)
+    RETURNS jwt_token
+    LANGUAGE 'plpgsql'
+    VOLATILE SECURITY DEFINER
+    PARALLEL UNSAFE
+    COST 100
+AS $BODY$
 DECLARE
   _role NAME;
+  user_role INTEGER;
   result jwt_token;
 BEGIN
-  SELECT users.username FROM users WHERE users.username=login_admin.username AND password=login_admin.upassword INTO _role;
+  SELECT users.username, users.role INTO _role, user_role FROM users WHERE users.username=login_admin.username AND password=login_admin.upassword;
   IF _role IS NULL THEN
     RAISE invalid_password USING message = 'invalid user or password';
   END IF;
+  
+  IF user_role = 2 THEN
+  	RAISE invalid_password USING message = 'this user does not exists';
+	END IF;
+  
+  IF user_role = 1 THEN
+    SELECT sign(
+      row_to_json(r), current_setting('app.settings.jwt_secret')
+    ) AS token
+    from (
+      SELECT 'kitchen_role' AS role, _role AS username,
+         extract(epoch from now())::integer + 3600*60*60 as exp
+    ) r
+    INTO result;
 
-  SELECT sign(
+  	RETURN result;
+  END IF;
+  
+  IF user_role = 0 THEN
+    SELECT sign(
       row_to_json(r), current_setting('app.settings.jwt_secret')
     ) AS token
     from (
@@ -49,6 +71,9 @@ BEGIN
     ) r
     INTO result;
 
-  RETURN result;
+  	RETURN result;
+  END IF;
+
+  
 END;
-$$
+$BODY$;
