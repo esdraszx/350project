68cc6a8c35efa4d9df56ee11712d38d161c91b4e
esdraszx <43818958+esdraszx@users.noreply.github.com>
2021-03-16 13:01:51 -0600

    Update signup.sql

--- signup.sql
+++ signup.sql
@@ -4,3 +4,13 @@ AS $$
   INSERT INTO users (username, password, name) VALUES
     (signup.username, signup.password, signup.name);
 $$ LANGUAGE sql SECURITY DEFINER;
+
+
+-- HELPER TO DELETE A SINGLE USER
+CREATE OR REPLACE FUNCTION
+delete_single_user(username TEXT) RETURNS VOID
+AS $$
+  UPDATE fridge SET username = '' WHERE fridge.username=delete_single_user.username;
+  UPDATE pantry SET username = '' WHERE pantry.username=delete_single_user.username;
+  DELETE FROM users WHERE username = delete_single_user.username;
+$$ LANGUAGE sql SECURITY DEFINER;
