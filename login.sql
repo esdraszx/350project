70a25a365c1d5e02a6ca4a5e26580a5f5e1fbe88
esdraszx <eesc.gg@gmail.com>
2021-03-17 15:35:03 -0600

    Added permissions

--- login.sql
+++ login.sql
@@ -25,22 +25,38 @@ END;
 $$
 
 
--- ADMIN LOGIN
+-- ADMIN LOGIN IS BEING USED FOR EVERY LOGIN
 
-CREATE OR REPLACE FUNCTION
-login_admin(username TEXT, upassword TEXT) RETURNS jwt_token
-  LANGUAGE plpgsql SECURITY DEFINER
-  AS $$
+CREATE OR REPLACE FUNCTION public.login_admin(IN username text,IN upassword text)
+    RETURNS jwt_token
+    LANGUAGE 'plpgsql'
+	SECURITY DEFINER
+AS $$
 DECLARE
   _role NAME;
+  user_role INTEGER;
   result jwt_token;
 BEGIN
-  SELECT users.username FROM users WHERE users.username=login_admin.username AND password=login_admin.upassword INTO _role;
+  SELECT users.username, users.role INTO _role, user_role FROM users WHERE users.username=login_admin.username AND password=login_admin.upassword;
   IF _role IS NULL THEN
     RAISE invalid_password USING message = 'invalid user or password';
   END IF;
+  
+  IF user_role = 1 THEN
+    SELECT sign(
+      row_to_json(r), current_setting('app.settings.jwt_secret')
+    ) AS token
+    from (
+      SELECT 'kitchen_role' AS role, _role AS username,
+         extract(epoch from now())::integer + 3600*60*60 as exp
+    ) r
+    INTO result;
 
-  SELECT sign(
+  	RETURN result;
+  END IF;
+  
+  IF user_role = 0 THEN
+    SELECT sign(
       row_to_json(r), current_setting('app.settings.jwt_secret')
     ) AS token
     from (
@@ -49,6 +65,9 @@ BEGIN
     ) r
     INTO result;
 
-  RETURN result;
+  	RETURN result;
+  END IF;
+
+  
 END;
-$$
+$$;
